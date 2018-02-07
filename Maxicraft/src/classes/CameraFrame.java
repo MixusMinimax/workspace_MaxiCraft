d936a527c53267c835224269de91d99fbf6bf30a
Barmetler.Maximilian <Barmetler.Maximilian@SK-901B0E969889.2817.skool.net>
2018-02-07 11:31:35 +0100

    asd

--- Maxicraft/src/classes/CameraFrame.java
+++ Maxicraft/src/classes/CameraFrame.java
@@ -94,7 +94,6 @@ public class CameraFrame extends Application {
 				}
 			}
 		}
-
 	}
 
 	@Override
--- Maxicraft/src/classes/MC.java
+++ Maxicraft/src/classes/MC.java
@@ -2,9 +2,12 @@ package classes;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
-import javafx.scene.input.KeyCode;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MC extends Application {
@@ -12,6 +15,19 @@ public class MC extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Group root = new Group();
+		
+		Pane pane = new Pane();
+		pane.getChildren().addAll(bGroup);
+
+		Scene scene = new Scene(pane, 800, 800, true, SceneAntialiasing.BALANCED);
+		scene.setFill(Color.rgb(20, 4, 40));
+
+		primaryStage.setScene(scene);
+		primaryStage.centerOnScreen();
+		primaryStage.setFullScreenExitHint("Press F11 to toggle Fullscreen.");
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+		}
 	}
 	
 	public void main(String[] args) {
