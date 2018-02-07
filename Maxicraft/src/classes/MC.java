3df88329c1ec94b1060ece5c46e81a132d0172da
Barmetler.Maximilian <Barmetler.Maximilian@SK-901B0E969889.2817.skool.net>
2018-02-07 10:56:08 +0100

    Glied

---
+++ Maxicraft/src/classes/MC.java
@@ -0,0 +1,16 @@
+package classes;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MC extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+	
+	public void main(String[] args) {
+		launch(args);
+	}
+}
